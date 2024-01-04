4ee7189b323e4a3b481327f1d5f48cad396bad33
Martin Avelen <martinboyb@gmail.com>
2024-01-03 21:41:12 -0300

    Add comentarios de la clase10 y a readme

--- project/cliente/views.py
+++ project/cliente/views.py
@@ -1,4 +1,9 @@
+#El modulo "views.py" trabaja la logica interna, se vincula views con "models.py" 
+# para trabajar con esos modelos. Tambien esta vinculado a "index.html" donde se muestran los templates
+
 from django.shortcuts import render
+
+#Se importa Cliente para usar el modelo
 from .models import Cliente
 
 
@@ -8,3 +13,4 @@ from .models import Cliente
 def index(request):
     clientes = Cliente.objects.all()
     return render(request, "cliente/index.html", {"clientes": clientes})
+                                                 #clientes es la key que usamos para iterar en el "index.html"
